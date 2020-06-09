48d868f4867e4fc62a142076650e7fe2af657ee0
Kyle Hannon <khannon@cocc.edu>
2020-06-09 09:29:49 -0700

    Reseting a board test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,4 +5,13 @@ describe 'A Tic Tac Toe board' do
 		board = Board.new()
 		expect(board.to_s).to eq(board.empty_locations)
 	end
+	it 'has an empty baord' do
+		board = Board.new()
+		expect(board.empty?).to be_truthy
+	end
+	it 'resets a board' do
+		board = Board.new() do
+			expect(board.reset).to be_truthy
+		end
+	end
 end	
