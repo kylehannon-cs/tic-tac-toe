10c7918aa06059863fe6566b932e63c31788f761
Kyle Hannon <khannon@cocc.edu>
2020-06-09 09:45:31 -0700

    places a token on board'

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,25 @@
 require_relative '../lib/board'
 
 describe 'A Tic Tac Toe board' do
+
+  let(:full_board) {
+    Board.new({
+      top: {left: :fake, middle: :fake, right: :fake},
+      middle: {left: :fake, middle: :fake, right: :fake},
+      bottom: {left: :fake, middle: :fake, right: :fake}
+    })
+  }
+
+  let(:midgame_board) {
+    Board.new({
+      top: {left: :o, middle: nil, right: nil},
+      middle: {left: nil, middle: :x, right: nil},
+      bottom: {left: nil, middle: nil, right: nil}
+    })
+  }
+
+
+
 	it '#to_s' do
 		board = Board.new()
 		expect(board.to_s).to eq(board.empty_locations)
@@ -10,8 +29,16 @@ describe 'A Tic Tac Toe board' do
 		expect(board.empty?).to be_truthy
 	end
 	it 'resets a board' do
-		board = Board.new() do
-			expect(board.reset).to be_truthy
-		end
+		board = Board.new(:midgame_board)
+		expect(board.reset).to be_truthy
 	end
-end	
+	it '#token_at' do
+		board = Board.new()
+		expect(board.token_at(2, 2)).to eq(nil)
+	end
+	it 'places a token at 2, 2' do
+		board = Board.new()
+		board.place('z', 2, 2)
+		expect(board.token_at(2,2)).to eq('[2,2]')
+	end
+end
